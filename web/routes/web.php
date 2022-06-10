27fd4ff266f2cd73ba012611a622305b7b62d4d6
yuukioda999 <yuuki46yuuki@icloud.com>
2022-06-10 15:54:06 +0900

    dockerデータベースの変更、フォルダー毎のタスクの一覧表示画面作成、テストデータ埋め込み表示のテスト

--- web/routes/web.php
+++ web/routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/folders/{id}/tasks', 'App\Http\Controllers\TaskController@index')->name('tasks.index');
